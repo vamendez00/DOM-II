9429fa327b0057bd7cce68bca58db188e6b95d2b
vamendez00 <veromendez@gmail.com>
2021-03-10 03:13:59 -0600

    2.1.2 update

--- js/index.js
+++ js/index.js
@@ -1,13 +1,44 @@
 // Your code goes here
 console.log (document);
 
+//1 - mouseover (change heading on mouseover)
 let logoHeader = document.querySelector(".logo-heading");
-console.log("myLogoHeading:", logoHeader);
+// console.log("myLogoHeading:", logoHeader);
 
-let myButton = document.querySelectorAll(".destination");
-console.log("myButtons:", myButton);
+logoHeader.addEventListener("mouseover", function(event){
+    logoHeader.textContent = "THE WHEELS ON THE BUS GO ROUND AND ROUND!";
+})
+
+//2 - mouseover (change section background colors as you move through page)
+let pageSections = document.querySelectorAll("section");
+console.log ("pageSections:", pageSections);
 
 
-// document.addEventListener("click", function (event){
-//     myButton[0].textContent = "Submitted!";
-// })
+//3 - click (change text on submit buttons)/change section background color
+let pageButtons = document.querySelectorAll(".btn");
+console.log("pageButtons:", pageButtons);
+
+let destinationSections = document.querySelectorAll(".destination");
+
+pageButtons[0].addEventListener("click", function(event){
+    pageButtons[0].textContent = "WANDERLUST!";
+    pageButtons[0].style.fontSize = "2.5rem";
+    pageButtons[0].style.fontWeight = "bold";
+    destinationSections[0].style.backgroundColor = "orange";
+})
+
+pageButtons[1].addEventListener("click", function(event){
+    pageButtons[1].textContent = "NOTHING BUT UP!";
+    pageButtons[1].style.fontSize = "2.5rem";
+    pageButtons[1].style.fontWeight = "bold";
+    destinationSections[1].style.backgroundColor = "purple";
+})
+
+pageButtons[2].addEventListener("click", function(event){
+    pageButtons[2].textContent = "ADVENTURE TIME!";
+    pageButtons[2].style.fontSize = "2.5rem";
+    pageButtons[2].style.fontWeight = "bold";
+    destinationSections[2].style.backgroundColor = "hotpink";
+})
+
+
