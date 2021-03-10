363390da6b5fe1c4d7f41d1ddc79e462e5751681
vamendez00 <veromendez@gmail.com>
2021-03-10 01:08:24 -0600

    2.1.2 Module Project Submission

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+console.log (document);
+
+let logoHeader = document.querySelector(".logo-heading");
+console.log("myLogoHeading:", logoHeader);
+
+let myButton = document.querySelectorAll(".destination");
+console.log("myButtons:", myButton);
+
+
+// document.addEventListener("click", function (event){
+//     myButton[0].textContent = "Submitted!";
+// })
